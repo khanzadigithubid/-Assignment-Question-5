1e9c42459418ee2b6594f476bd3ca4d7d1171199
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-05-14 14:46:40 +0500

    This is  Assignment Question 5
    This is  Assignment Question 5

---
+++ main.ts
@@ -0,0 +1,48 @@
+// 5. Calculator project using function:
+
+// .  Set up two variables containing number value.
+// .  Set up a variables to hold an operator (+ o -).
+// .  Create a function that takes two numbers and an operator as parameters, performs the corresponding operation, and returns the result.
+// .  Call the function with the variables and operator, and output the result using console.lig.
+// .  Update the operator vlue and call the function again with the update arguments.
+
+import inquirer from "inquirer";
+let user =await inquirer.prompt([
+    {
+        name: "firstNumber",
+        type: "number",
+        message: "Enter first number",
+    },
+    {
+        name: "secondNumber",
+        tye: "number",
+        message: "Enter second number",
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Select one of the operator to perform operation.",
+        choices: ["Addition", "Subtraction"],
+    },
+]);
+
+let {firstNumber, secondNumber, operator} = user;
+function num(num1:number, num2:number, operation:string) {
+    switch (operation) {
+        case "Addition":
+            return `The sum of ${num1} && ${num2} is ${num1 + num2}`;
+        break;
+
+        case "Subtraction":
+            return `The sum of ${num1} && ${num2} is ${num1 - num2}`;
+        break;
+
+    default: {
+        console.log("Please select a valid number");
+    }
+    };
+};
+let response= num(firstNumber, secondNumber, operator);
+console.log(response);
+
+
